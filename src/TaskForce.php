6224b3e1fd1f421668ad489fca3a8ae49ade181f
IhorBoiar <53617394+IhorBoiar@users.noreply.github.com>
2019-11-13 14:00:23 +0200

    Task 1 added in git for code-review

---
+++ src/TaskForce.php
@@ -0,0 +1,101 @@
+<?php
+
+namespace Htmlacademy;
+
+require "vendor/autoload.php";
+
+
+class StatusException extends Exception
+{
+    protected $message = 'Такого статуса нету';
+}
+
+class Task
+{
+    public $statusNow;
+    public $myAction;
+    public $statusNext = [];
+
+    const STATUS_NEW = 1;
+    const STATUS_CANCELED = 0;
+    const STATUS_IN_WORK = 10;
+    const STATUS_DONE = 200;
+    const STATUS_FAILED = 404;
+
+    const ACTION_RESPOND = 10;
+    const ACTION_CANCEL = 0;
+    const ACTION_DONE = 200;
+    const ACTION_REFUSE = 404;
+
+    const ROLE_ADMIN = 2;
+    const ROLE_USER = 1;
+    const ROLE_GUEST = 0;
+
+    public static $statusName = [
+        self::STATUS_NEW => 'Новое',
+        self::STATUS_CANCELED => 'Отменено',
+        self::STATUS_IN_WORK => 'В работе',
+        self::STATUS_DONE => 'Выполнено',
+        self::STATUS_FAILED => 'Провалено',
+    ];
+
+    public static $actionName = [
+        self::ACTION_RESPOND => 'Откликнуться',
+        self::ACTION_CANCEL => 'Отменить',
+        self::ACTION_DONE => 'Выполнено',
+        self::ACTION_REFUSE => 'Отказаться',
+    ];
+
+    public static $roleName = [
+        self::ROLE_ADMIN => 'Администратор',
+        self::ROLE_USER => 'Пользователь',
+        self::ROLE_GUEST => 'Гость',
+    ];
+
+    public function __construct($statusNow ? $statusNow : $statusName[self::STATUS_NEW]) {
+          if(!in_array($statusNow, self::statusName)) {
+                    throw new StatusException;
+                }
+        $this->statusNow = $statusNow;
+    }
+
+    public function nextStatus($myAction, $statusNow) {
+        if(!in_array($statusNow, self::statusName)) { /// EXCEPTION
+            throw new StatusException;
+        }
+        if(!in_array($myAction, self::actionName)) { /// EXCEPTION
+            throw new StatusException;
+        }
+        foreach (self::statusName as $status) {
+            foreach (self::actionName as $action) {
+                if ($statusNow == $status[self::STATUS_NEW]) { /// 1 New
+                    if($myAction == $action[self::ACTION_CANCEL]) {  /// 0 'cancel'
+                        return $this->statusNext = $status[self::STATUS_CANCELED]; /// 0 "Canceled"
+                    }
+                    if($myAction == $action[self::ACTION_RESPOND]) { /// 10 'respond'
+                        return $this->status_next = $status[self::STATUS_IN_WORKING];  /// 10 "In working"
+                    }
+                }
+                if ($statusNow == $status[self::STATUS_IN_WORK]) {   /// 10 'In working'
+                    if($myAction == $action[self::ACTION_DONE]) {  /// 200 'done'
+                        return $this->status_next = $status[self::STATUS_DONE];  /// 200 "Done"
+                    }
+                    if($myAction == $action[self::ACTION_REFUSE]) {   /// 404 'refuse'
+                        return $this->status_next = $status[self::STATUS_FAILED]; /// 404 "Failed"
+                    }
+                }
+            }
+        }
+    }
+    public function getStatus() {
+        return $this->statusNext;
+    }
+}
+
+try {
+    $task = new Task('Новое');
+    $task->nextStatus('Откликнуться');
+    echo $task->getStatus();
+} catch (StatusException $e) {
+    die($e->getMessage());
+}
